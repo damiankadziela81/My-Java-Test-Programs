7e7f9a21cb501d758fb9532028d46e9be4b33264
Damian Kadziela <damiankadziela02@gmail.com>
2022-06-18 19:06:21 +0200

    added array of tanks with constructor, moved caclulate volume method to Tank class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,38 +7,55 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String tempString;
-        Tank tank = new Tank();
+        Tank tank1 = new Tank(0,0,0,0);
+        Tank tank2 = new Tank(0,0,0,0);
+        Tank tank3 = new Tank(0,0,0,0);
+        Tank[] tankArray = {tank1,tank2,tank3};
 
-        JOptionPane.showMessageDialog(null, "Obliczmy poziom cieczy w zbiorniku (cysterna).");
-        // zbieranie danych i kontrola czy uzytkownik podal liczbe rzeczywista
+        JOptionPane.showMessageDialog(null, "Obliczmy poziom cieczy w 3 zbiornikach (cysternach).");
 
-        do {
-            tempString = JOptionPane.showInputDialog("Podaj dlugosc zbiornika [m]");
-        } while (!verify(tempString));
-        tank.lenght = Double.parseDouble(tempString);
+        for (int i = 0; i < 3; i++) {
+            double lenght;
+            double radius;
+            double fluidLevel;
+            String tempString;
 
-        do {
-            tempString = JOptionPane.showInputDialog("Podaj srednice zbiornika [m]");
-        } while (!verify(tempString));
-        tank.radius = Double.parseDouble(tempString) / 2;
+            // zbieranie danych i kontrola czy uzytkownik podal liczbe rzeczywista
 
-        do {
             do {
-                tempString = JOptionPane.showInputDialog("Podaj wysokosc cieczy w zbiorniku [m]");
+                tempString = JOptionPane.showInputDialog("Podaj dlugosc zbiornika nr "+(i+1)+" [m]");
             } while (!verify(tempString));
-            tank.fluidLevel = Double.parseDouble(tempString);
-            if (tank.fluidLevel > (2 * tank.radius)) {
-                JOptionPane.showMessageDialog(null, "Poziom nie moze przekraczac srednicy zbiornika.");
-            }
-        } while (tank.fluidLevel > (2 * tank.radius));
+            lenght = Double.parseDouble(tempString);
 
+            do {
+                tempString = JOptionPane.showInputDialog("Podaj srednice zbiornika nr "+(i+1)+" [m]");
+            } while (!verify(tempString));
+            radius = Double.parseDouble(tempString) / 2;
 
-        // obliczenia pojemnosci plynu w cysternie
-        double volume;
-        volume=calculateVolume(tank.lenght, tank.radius, tank.fluidLevel);
-        JOptionPane.showMessageDialog(null, "Dla L=" + tank.lenght + " R=" + tank.radius + " H=" + tank.fluidLevel + "\nObjetosc plynu w zbiorniku to " + volume + " m3");
+            do {
+                do {
+                    tempString = JOptionPane.showInputDialog("Podaj wysokosc cieczy w zbiorniku "+(i+1)+" [m]");
+                } while (!verify(tempString));
+                fluidLevel = Double.parseDouble(tempString);
+                if (fluidLevel > (2 * radius)) {
+                    JOptionPane.showMessageDialog(null, "Poziom nie moze przekraczac srednicy zbiornika.");
+                }
+            } while (fluidLevel > (2 * radius));
+
+            tankArray[i].lenght = lenght;
+            tankArray[i].radius = radius;
+            tankArray[i].fluidLevel = fluidLevel;
+        }
+
+        // obliczenia pojemnosci plynu w cysternach
+        double arrayVolume=0;
+        for (int i = 0; i < 3; i++) {
+            tankArray[i].calculateVolume();
+            arrayVolume=arrayVolume+tankArray[i].fluidVolume;
+            JOptionPane.showMessageDialog(null, "Dla L=" + tankArray[i].lenght + " R=" + tankArray[i].radius + " H=" + tankArray[i].fluidLevel + "\nObjetosc plynu w zbiorniku nr "+(i+1)+" to " + tankArray[i].fluidVolume + " m3");
+        }
+
+        JOptionPane.showMessageDialog(null, "Objetosc plynu w zbiornikach to " + arrayVolume + " m3");
     }
 
     //funkcja weryfikujaca czy string to liczba rzeczywista dodatnia
@@ -70,21 +87,5 @@ public class Main {
         System.out.println("---------------");
         return result;
     }
-
-    static double calculateVolume (double lenght,double radius, double level){
-        double a;
-        double b;
-        double c;
-        if (level <= radius) {
-            a = 1;
-            b = level;
-            c = 0;
-        } else {
-            a = -1;
-            b = 2 * radius - level;
-            c = Math.PI * radius * radius * lenght;
-        }
-        return a * lenght * (radius * radius * Math.acos((radius - b) / radius) - (radius - b) * Math.sqrt(b * (2 * radius - b))) + c;
-        }
 
 }
--- src/main/java/org/example/Tank.java
+++ src/main/java/org/example/Tank.java
@@ -4,4 +4,31 @@ public class Tank {
     double lenght;
     double radius;
     double fluidLevel;
+    double fluidVolume;
+    Tank(double lenght, double radius, double fluidLevel,double fluidVolume){
+        this.lenght = lenght;
+        this.radius = radius;
+        this.fluidLevel = fluidLevel;
+        this.fluidVolume= fluidVolume;
+    }
+
+    void calculateVolume(){
+        double a;
+        double b;
+        double c;
+        if (fluidLevel <= radius) {
+            a = 1;
+            b = fluidLevel;
+            c = 0;
+        } else {
+            a = -1;
+            b = 2 * radius - fluidLevel;
+            c = Math.PI * radius * radius * lenght;
+        }
+        this.fluidVolume = a * lenght * (radius * radius * Math.acos((radius - b) / radius) - (radius - b) * Math.sqrt(b * (2 * radius - b))) + c;
+        System.out.println(this.fluidVolume);
+        System.out.println("++++++++++++++++++++++++++++++++");
+
+    }
+
 }
